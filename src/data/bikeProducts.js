d36af94e6e8bb2b34f1979c017443627e66cfa5d
Anitha <anithagunasekar4@gmail.com>
2026-06-03 10:01:55 +0530

    Corrected

--- src/data/bikeProducts.js
+++ src/data/bikeProducts.js
@@ -2,7 +2,7 @@ import helmetImg from "../assets/images/bike/helmet1-men.jpeg";
 import jacketImg from "../assets/images/bike/riding-gear1.jpeg";
 import glovesImg from "../assets/images/bike/gloves1.jpeg";
 import HolderImg from "../assets/images/bike/phone-holder.jpeg";
-import USBChargerImg from "../assets/images/bike/usb-Charger.jpeg";
+import USBChargerImg from "../assets/images/bike/usb-charger.jpeg";
 import PhonemountImg from "../assets/images/bike/phone-mount.jpeg";
 
 export const bikeProducts = [
